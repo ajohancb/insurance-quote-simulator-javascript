ae0f0cbbd6bc391936fc3405c978be37ee849457
Johan Bravo <119433130+ajohancb@users.noreply.github.com>
2023-01-03 07:25:46 -0500

    Add files

---
+++ js/app.js
@@ -0,0 +1,76 @@
+// constructores
+
+function Seguro(marca, year, tipo) {
+    this.marca = marca
+    this.year = year
+    this.tipo = tipo
+}
+
+function UI() { }
+
+// Llena las opciones de los años
+
+UI.prototype.llenarOpciones = () => {
+    const max = 2020, min = max - 20
+    const selectYear = document.querySelector('#year')
+    for (let i = max; i > min; i--) {
+        let option = document.createElement('option')
+        option.value = i
+        option.textContent = i
+        selectYear.appendChild(option)
+    }
+}
+
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div')
+
+    if(tipo === 'error'){
+        div.classList.add('error')
+    } else {
+        div.classList.add('correcto')
+    }
+
+    div.classList.add('mensaje', 'mt-10')
+    div.textContent = mensaje
+
+    // Insertar en el html
+    const formulario = document.querySelector('#cotizar-seguro')
+    formulario.insertBefore(div, document.querySelector('#resultado'))
+
+    setTimeout(() => {
+        div.remove()
+    }, 3000);
+}
+
+// instanciar UI
+const ui = new UI()
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones() // Llena el select con los años
+})
+
+eventListeners()
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro')
+    formulario.addEventListener('submit', cotizarSeguro)
+}
+
+function cotizarSeguro(e){
+    e.preventDefault()
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value
+
+    // Leer el año seleccionado
+    const year = document.querySelector('#year').value
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value
+
+    if(marca === '' || year === '' || tipo === ''){
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error')
+        return
+    } 
+    ui.mostrarMensaje('Cotizando', 'exito')
+}
